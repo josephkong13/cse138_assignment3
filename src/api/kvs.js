c0463fdd25058fbea0db5937b04b6df2a405657e
unknown <jkong13@ucsc.edu>
2023-02-26 15:30:25 -0800

    added more to the endpoints

--- src/api/kvs.js
+++ src/api/kvs.js
@@ -24,7 +24,7 @@ router.put("/:key", (req, res) => {
 
   // if causal_metadata or val wasn't included in the body, send error
   if (!req.body || !req.body.val || !req.body["causal-metadata"]) {
-    res.status(400).json({ error: "bad PUT" });
+    res.status(400).json({ error: "bad request" });
     return;
   }
 
@@ -33,7 +33,7 @@ router.put("/:key", (req, res) => {
 
   // if val size more than 8MB, send error. may have to consider js objects
   if (val.length > 8000000) {
-    res.status(400).json({ error: "key or val too long" });
+    res.status(400).json({ error: "val too large" });
     return;
   }
 
@@ -46,6 +46,14 @@ router.put("/:key", (req, res) => {
 
   const last_written_vc = max_vc(causal_metadata, state.total_vc);
 
+  // If the key we write to was deleted or completely new, use response code 201
+  let response_code = 201;
+
+  // If we had an existing, non-deleted value, use response code 200
+  if (state.kvs.hasOwnProperty(key) && state.kvs[key] != null) {
+    response_code = 200;
+  }
+
   state.kvs[key] = {
     last_written_vc,
     value: val,
@@ -54,85 +62,101 @@ router.put("/:key", (req, res) => {
 
   // TODO: BROADCAST KVS + T_VC TO ALL OTHER REPLICAS IN VIEW
 
-  res.status(200).json({ "causal-metadata": last_written_vc });
-
-  // console.log(last_written_vc);
-
-  // for(const ip in state.total_vc) {
-  //   const causal_ip_val = causal_metadata[ip] ? causal_metadata[ip] : 0;
-  //   last_written_vc[ip] = Math.max(causal_ip_val, state.total_vc[ip]);
-  // }
-
-  // last_written_vc[address] = last_written_vc[address] + 1;
-
-  // // if thc is one behind, increment thc
-  // // if not dont inc
-  // // add to ops list
-
-  // // last_written_vc is always going to be newer than the total_vc
-  // let difference = 0;
-  // for(const ip in state.total_vc) {
-  //   difference = difference + last_written_vc[ip] - state.total_vc[ip]; 
-  // }
-
-  // if(difference == 1) {
-  //   state.total_vc
-  // }
-
-  // state.operations_list.push(last_written_vc);
-
-  // state.kvs[key] = {
-  //   last_written_vc,
-  //   value: val,
-  //   timestamp: new Date(),
-  // }
-
+  res.status(response_code).json({ "causal-metadata": last_written_vc });
 });
 
 // GET endpoint
 router.get("/:key", (req, res) => {
-  // if key wasn't included in the body, send error
-  if (!req.body.hasOwnProperty("key")) {
-    res.status(400).json({ error: "bad GET" });
+  // if we're uninitialized, return 418
+  if (!state.initialized) {
+    res.status(418).json({ error: "uninitialized" });
     return;
   }
 
-  let { key } = req.body;
-  // if hash map doesn't have the key, send not found error
-  if (!(key in kvs)) {
-    res.status(404).json({ error: "not found" });
+  const key = req.params.key;
+
+  // if causal_metadata wasn't included in the body, send error
+  if (!req.body || !req.body["causal-metadata"]) {
+    res.status(400).json({ error: "bad request" });
     return;
   }
-  // otherwise, send a successful response containing the value of the hash map's value
-  else {
-    let val = kvs[key];
-    res.status(200).json({ val: val });
-    return;
+
+  let causal_metadata = req.body["causal-metadata"];
+
+  if (kvs.hasOwnProperty(key)) {
+    let key_last_written = kvs[key].last_written_vc;
+    // compare_vc(key_last_written, causal_metadata);
+
+    // if key_last_written is newer/same/concurrent, return value
+    // and causal metadata = max_vc(causal_metadata, key_last_written)
+
   }
+
+  // compare_vc(state.total_vc, kvs[key].last_written_vc);
+
+  // if total_vc is newer or same
+  // return value, causal_metadata = max_vc(causal_metadata, key_last_written)
+
+  // otherwise, if total_vc is concurrent or older
+
+  // // if hash map doesn't have the key, send not found error
+  // if (!(key in kvs)) {
+  //   res.status(404).json({ error: "not found" });
+  //   return;
+  // }
+  // // otherwise, send a successful response containing the value of the hash map's value
+  // else {
+  //   let val = kvs[key];
+  //   res.status(200).json({ val: val });
+  //   return;
+  // }
 });
 
 // DELETE endpoint
-router.delete("/", (req, res) => {
-  // if key wasn't included in the body, send error
-  if (!req.body.hasOwnProperty("key")) {
-    res.status(400).json({ error: "bad DELETE" });
+router.delete("/:key", (req, res) => {
+
+  // if we're uninitialized, return 418
+  if (!state.initialized) {
+    res.status(418).json({ error: "uninitialized" });
     return;
   }
 
-  let { key } = req.body;
-  // if hash map doesn't have the key, send not found error
-  if (!(kvs in key)) {
-    res.status(404).json({ error: "not found" });
+  const key = req.params.key;
+
+  // if causal_metadata wasn't included in the body, send error
+  if (!req.body || !req.body["causal-metadata"]) {
+    res.status(400).json({ error: "bad request" });
     return;
   }
-  // otherwise, send a successful response containing the value of the hash map's value
-  else {
-    let old_val = kvs[key];
-    // no errors, delete the hash map's value
-    // kvs.delete(key);
-    res.status(200).json({ prev: old_val });
-    return;
+
+  let causal_metadata = req.body["causal-metadata"];
+
+  // last_written_vc = max of current thc and client, plus 1 for current write
+  if (state.total_vc.hasOwnProperty(full_address)) {
+    state.total_vc[full_address] += 1; 
+  } else {
+    state.total_vc[full_address] = 1; 
   }
+
+  const last_written_vc = max_vc(causal_metadata, state.total_vc);
+
+  // If the key we write to was deleted or doesn't exist, use response code 404
+  let response_code = 404;
+
+  // If we had an existing, non-deleted value, use response code 200
+  if (state.kvs.hasOwnProperty(key) && state.kvs[key] != null) {
+    response_code = 200;
+  }
+
+  state.kvs[key] = {
+    last_written_vc,
+    value: null,
+    timestamp: Date.now(),
+  }
+
+  // TODO: BROADCAST KVS + T_VC TO ALL OTHER REPLICAS IN VIEW
+
+  res.status(response_code).json({ "causal-metadata": last_written_vc });
 });
 
 module.exports = router;
